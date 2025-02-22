4135fb447bd4497b194ef0f1f60946cf50abd3b7
Abhishek Kumar <akumar.12121999@gmail.com>
2025-02-22 18:07:08 +0530

    Add files via upload

---
+++ A_to_z.java
@@ -0,0 +1,13 @@
+public class A_to_z{
+
+	public static void main(String [] args){
+
+		for(char latter = 'A' ; latter <= 'Z'; latter ++){
+
+			System.out.print(latter +" ");
+		}
+
+
+	}
+
+}
---
+++ Consonant_alphabets.java
@@ -0,0 +1,19 @@
+public class Consonant_alphabets{
+
+	public static void main(String [] args){
+
+		for(char latter = 'A' ; latter <= 'Z'; latter ++){
+			if( latter != 'A' && latter != 'E' && latter != 'I' && latter != 'O'  && latter != 'U'   ){
+
+			
+				System.out.print(latter +" ");
+
+			}
+
+			
+		}
+
+
+	}
+
+}
